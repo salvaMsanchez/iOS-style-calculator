20883bab87197afbc6a0141981a3821a8b7715a5
Salva Moreno <morenosanchezsalva@gmail.com>
2023-08-05 18:51:27 +0200

    Initial Commit

---
+++ iOS_Calculator/iOS_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CalculatorApp.swift
+//  iOS_Calculator
+//
+//  Created by Salva Moreno on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
